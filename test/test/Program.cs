0d60dc508a90611e61e9d8335b64b63134b7f6b5
Michal <misho.baran@gmail.com>
2012-08-10 07:08:13 +0200

    project VS 2010

---
+++ test/test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using mcMath;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable
+            long a, b, result;
+            a = 5;
+            b = 10;
+
+            //new obj
+            Class1 cls_obj = new Class1();
+               
+
+            //add
+            result = cls_obj.Add(a, b);
+
+            //print to console
+            Console.WriteLine(result.ToString() );
+
+            //only wait
+            Console.ReadLine();
+
+        }
+    }
+}
